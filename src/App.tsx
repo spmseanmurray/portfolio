ec8a26b544387ad7454591921c302f4005b3fcef
mgmer <disbesean@gmail.com>
2022-01-24 16:31:10 -0600

    improved content spacing

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,21 @@ import Skills from './Components/Skills';
 
 const App: React.FC = () => {
   return (
-    <div className="h-full bg-gray-900">
+    <div className=" bg-gray-900">
       <Landing />
-      <div className="flex flex-col">
-        <Header />
-        <About />
-        <Experience />
-        <Projects />
-        <Skills />        
-        <Footer />
+      <div className="flex flex-col relative">
+        <div className="fixed top-0 left-0 right-0">
+          <Header />
+        </div>
+        <div className="space-y-80 mb-80"> 
+          <About />
+          <Experience />
+          <Projects />
+          <Skills />
+        </div>
+        <div className="fixed bottom-0 left-0 right-0">
+          <Footer />
+        </div>
       </div>
     </div>
   );
