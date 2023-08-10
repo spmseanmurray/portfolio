5e6da1f528821df9c30730044c11da7eb9471dfc
Sean Murray <disbesean@gmail.com>
2023-08-10 11:58:46 -0500

    rewritting about me description

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -8,15 +8,18 @@ const About: React.FC = () => {
             <div className="flex flex-col lg:flex-row space-x-4 space-y-4 p-4 m-4 items-center border-2 bg-slate-800 border-slate-600 rounded-2xl">
                 <img className="w-3/5 sm:max-w-xs h-auto rounded-full border-2 border-black" src={ProfileImg} alt={"Profile"} />
                 <div className="max-w-prose text-slate-100">
-                    After internships at BMW and SpaceX and graduating Summa cum laude from North Carolina State University with a B.S. in Aerospace Engineering, 
-                    I started my professional career as a Jr. Design Engineer at Fathom5, a growth stage maritime cyber-security startup.
-                    While at Fathom5, I made the transition from hardware and mechanical engineering to full-stack software development. 
-                
-                    <div className='mt-4' />
-
-                    Following my time at Fathom5, I joined The Home Depot as a Software Engineer II where I am currently working in the marketing technology space.
+                    Hello, I'm Sean Murray—a proud graduate in Aerospace Engineering from North Carolina State University. 
+                    My journey began with notable internships at both BMW and SpaceX, kindling my intrigue for pushing technological boundaries and embracing novel challenges. 
+                    Following graduation, Austin, TX beckoned, leading me to immerse myself in the dynamic startup world at Fathom5, a maritime cyber-security venture. 
+                    This experience enabled me to seamlessly transition from my aerospace roots into the realm of full-stack software engineering.
+                <div className='mt-4' />
+                    Today my professional pursuits continue as a Software Engineer II at The Home Depot. 
+                    I dedicate myself to honing and expanding my technical expertise while steering the evolution of a supplier-centric retail media platform. 
+                    This platform orchestrates self-service marketing campaign launches, meticulous performance tracking, and streamlined advertisement management.
+                <div className='mt-4' />
+                    Beyond coding, you'll find me reading or fully immersed in the great outdoors. 
+                    Whether I'm hitting the pavement for a run, traversing hiking trails, or exploring the wilderness on backpacking trips, I relish every moment of adventure.
                 </div>
-
             </div>
         </div>
     );
